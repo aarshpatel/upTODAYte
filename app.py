2e431970c113e7c48689d6778cb7ab5ac6401bdc
Aarsh <aarsh@1x-nat-vl930-172-30-85-167.wireless.umass.edu>
2017-11-04 18:35:35 -0400

    Added more stuff to the maps

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, make_response
+from utils import *
+import pickle
+import json
+
 app = Flask(__name__)
+
+def get_stories():
+	new_stories = {}
+	with open("stories.json", "r") as f:
+		stories = json.load(f)
+	new_stories["data"] = stories
+	return new_stories
 
 @app.route('/')
 def main():
-	return render_template('index.html')
+	stories = get_stories()
+	return render_template('index.html', data=json.dumps(stories))
+	
 
 if __name__ == '__main__':
 	app.run(debug=True)
