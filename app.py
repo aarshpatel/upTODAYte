9acf54cc73f45dd08132dff038bf18cfc430aeb0
Aarsh <aarsh@Aarshs-MacBook-Pro.local>
2018-07-31 11:17:23 -0400

    Changed some stuff  in app.py

--- app.py
+++ app.py
@@ -6,29 +6,17 @@ import random
 
 app = Flask(__name__)
 
-def get_stories():
-    new_stories = {}
-    with open("stories.json", "r") as f:
-        stories = json.load(f)
-    new_stories["data"] = stories
-    return new_stories
-
-@app.route('/')
-def main():
-    stories = get_stories()
-    return render_template('index.html', data=json.dumps(stories))
-
 @app.route('/recap_me')
 def recap_me():
 	stories = get_stories()
 	all_summaries = []
 
-	cats = {
-			"Sports":  2,
-			"Tech":  2,
-			"Politics": 2,
-			"Celebrity Fan/Gossip": 2,
-			"Business": 2
+	categories = {
+            "Sports":  2,
+            "Tech":  2,
+            "Politics": 2,
+            "Celebrity Fan/Gossip": 2,
+            "Business": 2
 	}
 
 	for story in stories["data"]:
@@ -40,10 +28,10 @@ def recap_me():
 		for story_summary in summary:
 			if len(story_summary.split()) < 50 and len(story_summary.split()) > 10:
 				random_summary = story_summary
-		
-		if cats[story["category"]] != 0:	
+
+		if categories[story["category"]] != 0:
 			all_summaries.append((story["title"], story["img_url"], random_summary, story["link"]))
-			cats[story["category"]] -= 1
+			categories[story["category"]] -= 1
 
 
 		if sum(cats.values()) == 0:
@@ -53,6 +41,17 @@ def recap_me():
 
 	return render_template("recap_me.html", summaries=all_summaries)
 
+
+def get_stories():
+    new_stories = {}
+    with open("stories.json", "r") as f:
+        stories = json.load(f)
+    new_stories["data"] = stories
+    return new_stories
+
+def main():
+    stories = get_stories()
+    return render_template('index.html', data=json.dumps(stories))
 
 if __name__ == '__main__':
     app.run(debug=True)
